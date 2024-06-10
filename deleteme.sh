fa450e1772f3af6bd1660612d50b808459c83024
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2024-06-10 16:26:13 -0400

    R subreads

--- deleteme.sh
+++ deleteme.sh
@@ -1,7 +1,7 @@
-        STAR --runThreadN {4} \
+        STAR --runThreadN 4 \
             --genomeDir {/scratch/drt83172/Wallace_lab/RNA_SEQ/Genome/tall_fescuev0.1.fa} \
             --readFilesCommand zcat \
-            --readFilesIn CTE46P_O17_HP3_S356_F59_R1.fq.gz CTE46P_O17_HP3_S356_F59_R2.fq.gz \
-            --outFileNamePrefix CTE46P_O17_HP3_S356_F59_ \
+            --readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
+            --outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
             --limitBAMsortRAM 15000000000 \
             --outSAMtype BAM SortedByCoordinate
