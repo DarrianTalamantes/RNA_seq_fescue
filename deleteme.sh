49bb84a007497b7fbefe2d93482a5ed17c9ac272
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2024-06-10 16:39:03 -0400

    R subreads

--- deleteme.sh
+++ deleteme.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=4
 #SBATCH --mem 24gb
 #SBATCH -t 140:00:00
-#SBATCH --output=/scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/RNAseq.%j.out
-#SBATCH -e /scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/RNAseq.%j.err
+#SBATCH --output=/scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/Fixer.%j.out
+#SBATCH -e /scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/Fixer.%j.err
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user drt83172@uga.edu
 
@@ -16,7 +16,7 @@ echo "Done."
 ml STAR/2.7.10b-GCC-11.3.0     
     
         STAR --runThreadN 4 \
-            --genomeDir {/scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx} \
+            --genomeDir /scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx \
             --readFilesCommand zcat \
             --readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
             --outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
