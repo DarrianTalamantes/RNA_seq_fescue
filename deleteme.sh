880d1fed8f50269231e6c6c2671a0d6d5aa5e317
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2024-06-10 16:30:20 -0400

    R subreads

--- deleteme.sh
+++ deleteme.sh
@@ -1,5 +1,5 @@
         STAR --runThreadN 4 \
-            --genomeDir {/scratch/drt83172/Wallace_lab/RNA_SEQ/Genome} \
+            --genomeDir {/scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx} \
             --readFilesCommand zcat \
             --readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
             --outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
