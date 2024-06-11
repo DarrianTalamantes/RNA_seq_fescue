ca3cc3b344a2546d9f87fe0b5a8a10a45b04db99
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2024-06-11 10:46:04 -0400

    R subreads

--- deleteme.sh
+++ deleteme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J Fixer
 #SBATCH -p batch
-#SBATCH --ntasks=4
+#SBATCH --ntasks=32
 #SBATCH --mem 100gb
 #SBATCH -t 140:00:00
 #SBATCH --output=/scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/Fixer.%j.out
@@ -15,10 +15,10 @@ echo "Done."
 
 ml STAR/2.7.10b-GCC-11.3.0     
     
-        STAR --runThreadN 4 \
+        STAR --runThreadN 32 \
             --genomeDir /scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx \
             --readFilesCommand zcat \
             --readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
             --outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
-            --limitBAMsortRAM 15000000000 \
+            --limitBAMsortRAM 35000000000 \
             --outSAMtype BAM SortedByCoordinate
