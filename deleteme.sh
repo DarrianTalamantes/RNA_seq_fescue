4844c4ca653aff6d9f033ab408ab9633a84d82de
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2024-06-11 10:54:00 -0400

    R subreads

--- deleteme.sh
+++ deleteme.sh
@@ -14,11 +14,12 @@ sleep 5
 echo "Done."
 
 ml STAR/2.7.10b-GCC-11.3.0     
-    
-        STAR --runThreadN 32 \
-            --genomeDir /scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx \
-            --readFilesCommand zcat \
-            --readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
-            --outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
-            --limitBAMsortRAM 35000000000 \
-            --outSAMtype BAM SortedByCoordinate
+
+
+STAR --runThreadN 32 \
+--genomeDir /scratch/drt83172/Wallace_lab/RNA_SEQ/Genome_idx \
+--readFilesCommand zcat \
+--readFilesIn /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R1.fq.gz /scratch/drt83172/Wallace_lab/RNA_SEQ/Trimmed/CTE46P_O17_HP3_S356_F59_R2.fq.gz \
+--outFileNamePrefix /scratch/drt83172/Wallace_lab/RNA_SEQ/sep_bams/CTE46P_O17_HP3_S356_F59_ \
+--limitBAMsortRAM 35000000000 \
+--outSAMtype BAM SortedByCoordinate
