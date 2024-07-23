6650c0b9c5d64c1fef88f45e9b638017be5b5852
Darrian <darrianrtalamantes6@gmail.com>
2024-07-23 12:34:57 -0400

    work with subset of data now

--- Scripts/annoatations_script_eggnog.sh
+++ Scripts/annoatations_script_eggnog.sh
@@ -24,11 +24,13 @@ export LANG=en_SG.utf8
 # Objective: Take the output of tule annotation.smk and work with it.
 
 # Loading Modules
-module load InterProScan/5.68-100.0-foss-2022a
-
+module load eggnog-mapper/2.1.9-foss-2022a
 
 
         if [ ! -d /scratch/drt83172/Wallace_lab/RNA_SEQ/Annotation ]; then 
             mkdir -p /scratch/drt83172/Wallace_lab/RNA_SEQ/Annotation; 
         fi
-sh interproscan.sh -i /scratch/drt83172/Wallace_lab/RNA_SEQ/transcriptome/predicted_transcripts.fasta.transdecoder_dir/predicted_transcripts.fasta.clean.transdecoder.pep -f tsv -o /scratch/drt83172/Wallace_lab/RNA_SEQ/Annotation/interproscan_results.tsv -T /scratch/drt83172/Wallace_lab/RNA_SEQ/Annotation/interproscan_temp -cpu 24 -dp
+emapper.py -i /scratch/drt83172/Wallace_lab/RNA_SEQ/transcriptome/predicted_transcripts.fasta.transdecoder_dir/predicted_transcripts.fasta.clean.transdecoder_subset.pep --output /scratch/drt83172/Wallace_lab/RNA_SEQ/Annotations/predicted_transcripts.emapper.annotations --cpu 24 
+
+
+
