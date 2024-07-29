bb7faa5ce90a7bfe3e68f53f9cd8edf6cc28fba8
Darrian <darrianrtalamantes6@gmail.com>
2024-07-29 12:05:31 -0400

    Fixing names

---
+++ SRA_Downloader/Name_Fixer.sh
@@ -0,0 +1,17 @@
+# Author: Darrian Talamantes
+# Goal: rename the downloaded SRA's to the important name
+
+
+data_folder="/scratch/drt83172/Wallace_lab/RNA_SEQ/Data"
+List="/scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/RNA_seq_fescue/Lists/Fixed_Names_List.csv"
+
+
+while IFS=, read -r SRA NAME
+do
+    echo "Processing $data_folder/${SRA} with additional value $data_folder/${NAME}"
+
+#    mv $data_folder/${SRA} $data_folder/${NAME}
+#    mv $data_folder/${SRA} $data_folder/${NAME}
+        
+done < "$List"
+
