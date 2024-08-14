77705527430a1462286d9e67cdcbc6fe06327c4b
Darrian <darrianrtalamantes6@gmail.com>
2024-08-14 18:10:46 -0400

    working on making gtf files

--- Non_Pipeline/practice_deleteme.sh
+++ Non_Pipeline/practice_deleteme.sh
@@ -6,7 +6,7 @@ gff3_dir="/scratch/drt83172/Wallace_lab/RNA_SEQ/manual_annotation/gff3"
 transdecoder="/scratch/drt83172/Wallace_lab/RNA_SEQ/manual_annotation/transdecoder"
 
 
-for file in $(ls $small_gtf_dir | grep "dupped"); do
+for file in $(ls $small_gtf_dir | grep "dupped" | grep "PxH_NegxPos"); do
     base_name=$(basename "$file" | sed 's/\.[^.]*$//')
     echo $base_name
     # # Run bed tools (gtf to fasta)
@@ -27,4 +27,3 @@ for file in $(ls $small_gtf_dir | grep "dupped"); do
 done
 
 
-/scratch/drt83172/Wallace_lab/RNA_SEQ/transcriptome/predicted_transcripts.fasta.transdecoder_dir/predicted_transcripts.fasta.transdecoder_col3swap.pep
