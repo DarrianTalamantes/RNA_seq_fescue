b0685be4694ce5d91819ff17ffebd5fef7ed72e0
Darrian <darrianrtalamantes6@gmail.com>
2024-08-14 17:23:39 -0400

    working on making gtf files

--- Non_Pipeline/practice_deleteme.sh
+++ Non_Pipeline/practice_deleteme.sh
@@ -1,3 +1,10 @@
+# setting variables
+genome="/scratch/drt83172/Wallace_lab/RNA_SEQ/Genome/tall_fescuev0.1.fa"
+small_gtf_dir="/scratch/drt83172/Wallace_lab/RNA_SEQ/manual_annotation/small_gtfs"
+bedtools_dir="/scratch/drt83172/Wallace_lab/RNA_SEQ/manual_annotation/bedtools"
+transdecoder="/scratch/drt83172/Wallace_lab/RNA_SEQ/manual_annotation/transdecoder"
+
+
 for file in $(ls $small_gtf_dir); do
     base_name=$(basename "$file" | sed 's/\.[^.]*$//')
     print($base_name)
