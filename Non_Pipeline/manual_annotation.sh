ab3599f3e40d2754bb772fa451c28ba6bee2f5be
Darrian <darrianrtalamantes6@gmail.com>
2024-08-15 12:16:42 -0400

    getting transdecoder to work

--- Non_Pipeline/manual_annotation.sh
+++ Non_Pipeline/manual_annotation.sh
@@ -1,6 +1,23 @@
-# ml TransDecoder/5.7.0-GCC-11.3.0
-# ml BEDTools/2.30.0-GCC-12.2.0
-# ml InterProScan/5.68-100.0-foss-2022a
+#!/bin/bash
+#SBATCH -J small_gtf_ann
+#SBATCH -p batch
+#SBATCH --ntasks=8
+#SBATCH --mem 40gb
+#SBATCH -t 20:00:00
+#SBATCH --output=/scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/small_gtf_ann.%j.out
+#SBATCH -e /scratch/drt83172/Wallace_lab/RNA_SEQ/Scripts/outfiles/small_gtf_ann.%j.err
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user drt83172@uga.edu
+
+echo "This JobID for this job is ${SLURM_JOB_ID}."
+sleep 5
+echo "Done."
+
+sacct -j $SLURM_JOB_ID --format=JobID,JobName,AllocCPUS,Elapsed,ExitCode,State,MaxRSS,TotalCPU
+
+ml TransDecoder/5.7.0-GCC-11.3.0
+ml BEDTools/2.30.0-GCC-12.2.0
+ml InterProScan/5.68-100.0-foss-2022a
 
 
 # setting variables
@@ -35,8 +52,8 @@ for file in $(ls $small_gtf_dir | grep "dupped"); do
     
     # clean the pepfile
     sed 's/*//g' $transdecoder_output/$base_name.fa.transdecoder.pep > $transdecoder_output/$base_name.fa.transdecoder_clean.pep
-    export JAVA_OPTS="-Xmx6G"
-    interproscan.sh -cpu 2 -f TSV,GFF3 -goterms -b $interpro/$base_name -i $transdecoder_output/$base_name.fa.transdecoder_clean.pep -T $interpro_temp
+    export JAVA_OPTS="-Xmx10G"
+    interproscan.sh -cpu 8 -f TSV,GFF3 -goterms -b $interpro/$base_name -i $transdecoder_output/$base_name.fa.transdecoder_clean.pep -T $interpro_temp
 done
 
 
