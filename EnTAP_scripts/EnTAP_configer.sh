7cd7ac156e0640ecc77791d1c342cdd03a36c1e9
Darrian <darrianrtalamantes6@gmail.com>
2025-05-06 12:19:30 -0400

    entap time

--- EnTAP_scripts/EnTAP_configer.sh
+++ EnTAP_scripts/EnTAP_configer.sh
@@ -16,5 +16,5 @@ sacct -j $SLURM_JOB_ID --format=JobID,JobName,AllocCPUS,Elapsed,ExitCode,State,M
 ml EnTAP/2.2.0-GCCcore-11.3.0
 
 # Code to be Ran
-EnTAP --run --run-ini /scratch/drt83172/Wallace_lab/RNA_SEQ/EnTAP/Params/entap_run.params --entap-ini /scratch/drt83172/Wallace_lab/RNA_SEQ/EnTAP/Params/entap_config_ini
+EnTAP --run --run-ini /scratch/drt83172/Wallace_lab/RNA_SEQ/EnTAP/Params/entap_run.params --entap-ini /scratch/drt83172/Wallace_lab/RNA_SEQ/EnTAP/Params/entap_config.ini
 
